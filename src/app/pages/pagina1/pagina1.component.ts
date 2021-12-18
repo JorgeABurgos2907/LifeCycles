9ecbddee66030c1d21a9baf7c6d8936d37952d0c
Jorge Burgos <burgosjorge2907@gmail.com>
2021-12-17 21:12:33 -0500

    ngDestroy

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,4 +1,5 @@
 import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pagina1',
@@ -8,11 +9,18 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 })
 export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
+
+  nombre: string = 'Jorge';
+  segundos:number = 0;
+  timerSubscription!: Subscription;
+
+
   constructor() { 
     console.log('constructor')
   }
   ngOnDestroy(): void {
     console.log('Destroy');
+    this.timerSubscription.unsubscribe();
   }
   ngAfterViewChecked(): void {
     console.log('AfterViewChecked');
@@ -35,11 +43,14 @@ export class Pagina1Component implements OnInit, OnChanges, DoCheck, AfterConten
 
   ngOnInit(): void {
     console.log('ngOnit')
+    this.timerSubscription = interval(1000).subscribe( i => {
+      this.segundos = i;
+    })
   }
 
 
   guardar(){
-    
+
   }
   
 
